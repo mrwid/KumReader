ca48d0e6f9bc5dd490eb9378167671043a1b2879
mrwid <mr_wid@163.com>
2012-08-16 17:00:49 +0800

    启动界面添加自适应屏幕大小功能
    *启动界面自适应手机屏幕尺寸
    
    Mr.Wid
    ver:0.2.20120816.1659

--- kum_start.py
+++ kum_start.py
@@ -3,6 +3,8 @@
 import appuifw
 import graphics
 import sysinfo
+import topwindow
+import e32
 
 class NoStartImg(Exception):
     pass
@@ -16,19 +18,25 @@ class Start:
         try:
             self.ShowStartFrame()
         except NoStartImg:
-			appuifw.note(self.cn("应用程序已损坏,请重新安装！\nError:401"), "error")
-			appuifw.app.set_exit()
+            appuifw.note(self.cn("应用程序已损坏,请重新安装！\nError:401"), "error")
+            appuifw.app.set_exit()
 
     def ShowStartFrame(self):
-		try:
-			try:
-				_imgstart = graphics.Image.open(r"C:\python\images\KumReader_start.jpg")
-				self.gui.blit(_imgstart)
-			except SymbianError:
-				_imgstart = graphics.Image.open(r"D:\python\images\KumReader_start.jpg")
-				self.gui.blit(_imgstart)
-		except:
-			raise NoStartImg
+        try:
+            try:
+                _imgstart = graphics.Image.open(r"C:\python\images\KumReader_start.jpg")
+	    except SymbianError:
+                _imgstart = graphics.Image.open(r"D:\python\images\KumReader_start.jpg")
+            _imgW,imgH = _imgstart.size
+        except:
+            raise NoStartImg
+
+        screen = topwindow.TopWindow()
+        screen.add_image(_imgstart, (0, 0, self._screenW, self._screenH))
+        screen.size = (self._screenW, self._screenH)
+        screen.show()
+        e32.ao_sleep(2)
+        screen.hide()
         
     def cn(self, x):
         return x.decode("utf-8")
@@ -37,8 +45,8 @@ def test():
 	AppStart = Start()
 	
 	from e32 import Ao_lock
-	lock=Ao_lock()
-	appuifw.app.exit_key_handler=lock.signal
+	lock = Ao_lock()
+	appuifw.app.exit_key_handler = lock.signal
 	lock.wait()
 
 if __name__ == '__main__':
