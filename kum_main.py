3d152d2201dabe1b9b6de235acd075f76b6190d3
mrwid <mr_wid@163.com>
2012-08-22 18:20:01 +0800

    KumReader主程序框架
    *主程序的框架，待完善
    
    Mr.Wid

--- kum_main.py
+++ kum_main.py
@@ -3,25 +3,23 @@
 #Date: 2012.08.22
 #----------------
 
+import kum_start
 import appuifw
 import graphics
 import e32dbm
 import sysinfo
-import kum_start
+import powlite_fm
 
 class KumReader:
     def __init__(self):
         #初始化主程序
-        app = kum_start.Start()
-        del app
-        
         self.CheckSrc()     #检查资源文件
-        appuifw.body = appuifw.Canvas()
-        self.MainFrame = self.CreateFrame()     #创建主界面画布
-        self.ReadFrame = self.CreateFrame()     #创建阅读界面画图
-        appuifw.body = self.mainUI = appuifw.Canvas(self.main_redraw, self.main_event)     #主程序主界面
-        self.readUI = appuifw.Canvas(self.read_redraw, self.read_event)     #阅读界面
+        #kum_start.ShowStartFrame()      #显示软件启动界面
+        appuifw.app.screen = 'full'
+        appuifw.app.body = self.mainUI = appuifw.Canvas()     #主程序主界面
+        self.InitMainFrame()
 
+        
     #检查资源文件-----------
     def CheckSrc(self):
         try:
@@ -58,18 +56,12 @@ class KumReader:
             appuifw.note(self.cn('应用程序已损坏,请重新安装!'), 'error')
             appuifw.app.set_exit()
 
-    #创建画布-------------
-    def CreateFrame(self):
-        appuifw.app.screen = "full"
-        _w, _h = sysinfo.display_pixels()
-        return graphics.Image.new(size = (_w, _h), mode = 'L')
     
     #编码处理------------
     def cn(self, x):
         return x.decode('utf-8')
 
     #事件处理-----------
-    #----主界面事件处理----
     def main_redraw(self):
         try:
             self.mainUI.blit(self.MainFrame)
@@ -79,16 +71,34 @@ class KumReader:
     def main_event(self):
         pass
 
-    #----阅读界面事件处理----
-    def read_redraw(self):
-        try:
-            self.mainUI.blit(self.ReadFrame)
-        except:
-            pass
+    #文件选择-----------
+    def SelectFile(self):
+        path = powlite_fm.manager().AskUser('c:/', find = 'file')
+        return path
 
-    def read_event(self):
+    #初始化主界面------------
+    def InitMainFrame(self):
+        place = [(int(self.dbBookshelf['T0']), int(self.dbBookshelf['V0'])),
+                 (int(self.dbBookshelf['T1']), int(self.dbBookshelf['V0'])),
+                 (int(self.dbBookshelf['T2']), int(self.dbBookshelf['V0'])),
+                 (int(self.dbBookshelf['T0']), int(self.dbBookshelf['V1'])),
+                 (int(self.dbBookshelf['T1']), int(self.dbBookshelf['V1'])),
+                 (int(self.dbBookshelf['T2']), int(self.dbBookshelf['V1'])),
+                 (int(self.dbBookshelf['T0']), int(self.dbBookshelf['V2'])),
+                 (int(self.dbBookshelf['T1']), int(self.dbBookshelf['V2'])),
+                 (int(self.dbBookshelf['T2']), int(self.dbBookshelf['V2'])),
+            ]
+        font = (appuifw.available_fonts()[2], 11)
+        for i in range(9):
+            self.imgBookShelf.blit(self.imgBook, target = place[i])
+            _bookName = self.dbBookshelf[str(i)][19:-4]
+            self.imgBookShelf.text(place[i], self.cn(self.dbBookshelf[str(i)][19:]), (40,40,40),font )
+        self.mainUI.blit(self.imgBookShelf)
+
+    #显示图像-----------
+    def MakeImg(self, style):
         pass
-
+        
 
 def test():
     app = KumReader()
